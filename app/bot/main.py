9aad63e20267583b7c119d73b786937cad8f6779
Vasyl Radashev <vasiliyradashev24@gmail.com>
2026-03-17 20:17:48 +0100

    feat: stable docker-compose stack with poetry

--- app/bot/main.py
+++ app/bot/main.py
@@ -11,7 +11,6 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = Bot(token=BOT_TOKEN)
